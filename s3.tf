4b32e288805bbc3e599682168faf39e0283528b8
wasanmohit <89068807+wasanmohit@users.noreply.github.com>
2023-08-28 17:32:59 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,13 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-first-bucket"
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "test"
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
   }
 }
